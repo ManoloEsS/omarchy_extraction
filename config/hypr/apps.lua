d7ef015e92b261559c0670308a35e1206f154c38
Manolo Estrada S <manolo.estrada.s@gmail.com>
2026-08-28 11:58:51 -0600

    Match Omarchy TUI and power menu behavior

--- config/hypr/apps.lua
+++ config/hypr/apps.lua
@@ -3,6 +3,10 @@
 local o = require("hypr.helpers")
 
 o.window("(com[.]mitchellh[.]ghostty|org[.]desktop[.].*)", { tag = "+terminal" })
+o.window("^org[.]desktop[.](bluetui|impala|wiremix|btop)$", { tag = "+floating-window" })
+o.window({ tag = "floating-window" }, { float = true })
+o.window({ tag = "floating-window" }, { center = true })
+o.window({ tag = "floating-window" }, { size = "875 600" })
 o.window({ tag = "terminal" }, { tag = "-default-opacity" })
 o.window({ tag = "terminal" }, { opacity = "0.985 0.96" })
 
